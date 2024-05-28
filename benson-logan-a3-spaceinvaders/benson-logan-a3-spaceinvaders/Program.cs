4b9347a50727e1882ec10edf5a50ed294810b315
Logan Benson <129000785+Walacup@users.noreply.github.com>
2024-05-28 12:39:24 -0400

    Added ball object

--- benson-logan-a3-spaceinvaders/benson-logan-a3-spaceinvaders/Program.cs
+++ benson-logan-a3-spaceinvaders/benson-logan-a3-spaceinvaders/Program.cs
@@ -10,6 +10,11 @@ public class Program
     static int screenHeight = 600; // Screen height
     static int targetFps = 60; // Target frames-per-second
 
+    // Ball or "Bullet" variables
+    static int radius;
+    static Raylib_cs.Color color;
+    static Vector2 position;
+
     // Player ship location and speed
     static int shipX = 360;
     static int shipY = 400;
@@ -53,13 +58,21 @@ public class Program
     static void Setup()
     {
         // Your one-time setup code here
+        radius = 25;
+        position = new Vector2(400, radius);
+        color = Raylib_cs.Color.Gold;
+        
     }
 
     static void Update() 
     {
 
         // Your game code run each frame here
+
+        //Ball / laser / bullet
+        Raylib.DrawCircleV(position, radius, color);
         
+
         // Ship controls
         if (Raylib.IsKeyDown(KeyboardKey.Left) && shipX > 0)
         {
@@ -93,3 +106,4 @@ public class Program
         Raylib.DrawText("Time:", 15, 250, 30, Raylib_cs.Color.White);
     }
 }
+
